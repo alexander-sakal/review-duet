b6fbc88f14fa5b6a3949656cdc8e7f6b651473ca
Alexander Sakal <alexandersakal02@gmail.com>
2026-01-25 16:33:26 +0200

    build: add coroutines dependency for review diff view

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 
     intellijPlatform {
